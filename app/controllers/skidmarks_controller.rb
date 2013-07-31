31dcf2218b87385695e1896e1966abec2e618ae2
geordie <george.speake@gmail.com>
2013-07-31 16:45:13 -0700

    test app runs with some dummy data

--- app/controllers/skidmarks_controller.rb
+++ app/controllers/skidmarks_controller.rb
@@ -2,7 +2,7 @@ class SkidmarksController < ApplicationController
   before_filter :get_gem_version
 
   def index
-
+  	@jobs = [{:name => 'job 1', :cron => '* * * 10 20', :class => 'Array', :method => 'join', :args => [1,2], :for_each_vhost => true}, {:name => 'job 2', :cron => '15/3 * * 10 20', :class => 'Hash', :method => 'concat', :args => ['a', 'b'], :for_each_vhost => false}]
 
   end
 
@@ -10,6 +10,6 @@ class SkidmarksController < ApplicationController
   private
 
   def get_gem_version
-    @skidmards_gem_version = Gem.loaded_specs['skidmarks'].version.to_s
+    @skidmarks_gem_version = Gem.loaded_specs['skidmarks'].version.to_s
   end
 end
